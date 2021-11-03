59259d36ac5c585c73d749f17d189654d92c53e9
kennethmxp <kennethmxp@gmail.com>
2021-11-03 17:40:07 +0700

    Finish Exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,40 @@
-const add = function() {
-	
+const add = function(a,b) {
+  return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+  return a-b;	
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  let total=0;
+  arr.forEach(element => {
+    total+=element;
+    
+  });
+  return total;
+  	
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let result=1;
+  arr.forEach(element=>result*=element);
+  return result;
+  
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return a**b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  // 4x3x2x1
+  let result=1;
+  while(a>1) {
+    result*=a;
+    a--;
+  }	
+  return result;
 };
 
 // Do not edit below this line
